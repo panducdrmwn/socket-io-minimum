a10f814e723f9f58f6950847fe8b3a73fb01ff09
panducdrmwn <panducdrmwn@gmail.com>
2026-03-30 16:14:09 +0700

    Updates

---
+++ _next/static/-NIZm1IV2LIEGBim41JIo/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/socket-io-minimum//_next/:path+",
+        "destination": "/socket-io-minimum/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
