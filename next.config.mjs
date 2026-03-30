9c1dc0b1824b79c7c08c3142d263e0e7afccb3d5
panducdrmwn <panducdrmwn@gmail.com>
2026-03-30 16:08:29 +0700

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-   reactStrictMode: false,
+  reactStrictMode: false,
   trailingSlash: true,
   assetPrefix: isProd ? '/socket-io-minimum/' : '',
   basePath: isProd ? '/socket-io-minimum' : '',
-  output: 'export',
+  
 }
 
 export default nextConfig
